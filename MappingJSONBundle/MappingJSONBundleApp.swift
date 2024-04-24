50068fd94a56d859b53a6d4f3630e05fa91c98bd
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-24 08:45:32 +0700

    Initial Commit

---
+++ MappingJSONBundle/MappingJSONBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingJSONBundleApp.swift
+//  MappingJSONBundle
+//
+//  Created by MACBOOK PRO on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MappingJSONBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
